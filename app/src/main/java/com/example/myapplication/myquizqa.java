87ef7a35771f093600176748e0e1f8973ff3ad48
Shruti <shrutisinha7262@gmail.com>
2023-08-04 11:44:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/myquizqa.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+public class myquizqa {
+
+
+
+    public static String question[] ={
+
+            "Which is the longest river in the world?",
+            "Which is the largest ocean in the world?",
+            "Which is India’s first super computer?",
+            "Which bank is called bankers Bank of India?",
+            "Which is the largest animal in the world?",
+            "Which is largest animal on land?",
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which is the smallest country in the world?",
+            "Tsunami is a word in which language?"
+
+
+    };
+
+    public static String choices[][] = {
+            {"Great Ganga","Nile","Amazon", "Niger"},
+            {"Indian Ocean" ,"Pacific Ocean" , "Arctic Ocean", "Atlantic Ocean"},
+            {"Param8000" ,"param80000" , "param800" , "param8"},
+            {"Reserve Bank of India" ,"Punjab National Bank" ,"State Bank of India","ICICI Bank"},
+            {"Shark","Elephant","Blue whale", "Giraffe"},
+            {"Tiger","Lion","Zebra","American Elephant"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Vatican City","Bhutan","Nepal", "Shri Lanka"},
+            {"Hindi","Urdu","Japanese", "French"}
+    };
+
+    public static String correctAnswer[] = {
+            "Nile",
+            "Pacific Ocean",
+            "Param8000",
+            "Reserve Bank of India",
+            "Blue whale",
+            "American Elephant",
+            "Google",
+            "Notepad",
+            "Vatican City",
+            "Japanese"
+    };
+
+}
+
